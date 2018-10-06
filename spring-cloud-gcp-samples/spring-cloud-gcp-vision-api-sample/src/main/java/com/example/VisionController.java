ec103314eba7349407896b51bd629447b34a8be3
Mark Chmarny <mark@chmarny.com>
2018-10-06 19:11:22 +0100

    Removed confidence

--- spring-cloud-gcp-samples/spring-cloud-gcp-vision-api-sample/src/main/java/com/example/VisionController.java
+++ spring-cloud-gcp-samples/spring-cloud-gcp-vision-api-sample/src/main/java/com/example/VisionController.java
@@ -80,7 +80,7 @@ public class VisionController {
 
 		StringBuilder responseBuilder = new StringBuilder("<table border=\"1\">");
 
-		responseBuilder.append("<tr><th>label</th><th>score</th><th>confidence</th></tr>");
+		responseBuilder.append("<tr><th>label</th><th>score</th></tr>");
 
 		// We're only expecting one response.
 		if (responses.getResponsesCount() == 1) {
@@ -95,8 +95,6 @@ public class VisionController {
 						.append(annotation.getDescription())
 						.append("</td><td>")
 						.append(annotation.getScore())
-						.append("</td><td>")
-						.append(annotation.getConfidence())
 						.append("</td></tr>");
 			}
 		}
